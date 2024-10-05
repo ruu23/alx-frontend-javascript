cfe7cdc4a370332f6bf874f201aad3f7a327cb04
ruu23 <arwamah777@gmail.com>
2024-10-05 15:54:11 +0300

    create report

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,8 @@
 export default function createReportObject(employeesList) {
-
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: () => Object.keys(employeesList).length,
+  };
 }
