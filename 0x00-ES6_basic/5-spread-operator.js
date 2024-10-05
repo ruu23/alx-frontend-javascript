547c18450a0734d12f73dc8a0a0177168e0876c0
ruu23 <arwamah777@gmail.com>
2024-10-05 10:30:04 +0300

    spread operator

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const newStr = string.split('');
-  const newArr = [...array1, ...array2, ...newStr];
-  return newArr;
+  return [...array1, ...array2, ...string];
 }
