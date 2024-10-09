13ee9a648342ef126cfb09aa1abc187a43d5efc0
ruu23 <arwamah777@gmail.com>
2024-10-09 17:48:00 +0300

    value in array

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if(startString.length === 0){
+  if (startString.length === 0) {
     return '';
   }
-  const list = []
-  for(const item of set){
-    if(item && item.startsWith(startString)){
-      list.push(item.slice(startString.length))
+  const list = [];
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
     }
   }
-  return list.join('-')
+  return list.join('-');
 }
