14f3e87f034c3abb1077f4fc9ea6d68ffee0e1e6
ruu23 <arwamah777@gmail.com>
2024-12-05 20:45:01 +0200

    make simple promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
+  return promise
+    .then(() => {
       console.log('Got a response from the API');
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
