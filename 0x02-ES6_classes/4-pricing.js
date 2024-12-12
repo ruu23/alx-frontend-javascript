82aa63321eb736c0a9dc4ba428260a7ca9ad4964
ruu23 <arwamah777@gmail.com>
2024-12-13 01:20:22 +0200

    add pricing method

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this.amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()})`;
+    return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()})`;
   }
 
   static convertPrice(amount, conversionRate) {
