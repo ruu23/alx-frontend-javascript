d2c58f8cd05d287c909e9d77279a1311e66dd2ad
ruu23 <arwamah777@gmail.com>
2024-12-13 01:11:36 +0200

    add pricing method

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,7 +3,7 @@ import Currency from './3-currency';
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
-    this.currency = currency;
+    this._currency = currency;
   }
 
   get amount() {
