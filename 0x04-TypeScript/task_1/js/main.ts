70c3e15792017f3e0fb212e63a774e7e30b21ffc
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-09 11:34:16 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,7 +16,7 @@ interface printTeacherFunction{
 }
 
 function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
-    return`${firstName}.${lastName}`;
+    return`${firstName}. ${lastName}`;
 };
 
 interface StudentConstructor{
