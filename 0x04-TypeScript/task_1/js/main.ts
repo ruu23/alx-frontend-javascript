9f556dddcf4b2f652ecb63203c901e36800d1bd6
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-09 11:10:17 +0200

    Implemented Teacher interface with readonly firstName/lastName, fullTimeEmployee, optional yearsOfExperience, location, and arbitrary attributes.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,74 +2,44 @@ interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
+  yearsOfExperience?: Number;
   location: string;
-  yearsOfExperience?: number;
   [key: string]: any;
+};
+
+interface Director extends Teacher{
+    numberOfReports: Number;
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+interface printTeacherFunction{
+    (firstName: string, lastName: string): string;
 }
 
-interface printTeacherFunction {
-  (firstName: string, lastName:string): string
-}
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName[0]}.${lastName}`
-}
-
-interface StudentConstructor {
-  firstName: string;
-  lastName: string;
-}
-
-interface StudentInterface {
-  firstName: string;
-  lastName: string;
-  workOnHomework(): string;
-  displayName(): void;
-}
-
-class StudentClass implements StudentInterface {
-  firstName: string;
-  lastName: string;
-
-  constructor({firstName, lastName}: StudentConstructor) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return "Currently working";
-  }
-
-  displayName(): void {
-    this.firstName;
-  }
-}
-
-const teacher1: Teacher = {
-  firstName: 'arwa',
-  lastName: 'Mahmoud',
-  fullTimeEmployee: false,
-  location: 'Mansoura',
-  yearsOfExperience: 2,
-  contract: true,
-}
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
+    return`${firstName.charAt(0)}.${lastName}`;
 };
-console.log(teacher3);
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
+interface StudentConstructor{
+    firstName: string;
+    lastName: string;
+}
+
+interface studentInterface{
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements studentInterface{
+    firstName:string;
+    lastName:string;
+    constructor({firstName, lastName}: StudentConstructor){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework():string {
+        return 'Currently working'
+    }
+    displayName():string {
+        return this.firstName
+    }
+}
