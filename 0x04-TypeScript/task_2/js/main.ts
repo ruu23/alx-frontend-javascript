bd872210694c911aebbfc8b47b1c632870d8b7f4
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-10 14:27:23 +0200

    Add TypeScript setup for employee creation logic

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,52 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+class Director implements  DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+function createEmployee(salary: number | string): Teacher | Director {
+    if ( typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
+
+console.log(createEmployee(200));    // Teacher
+console.log(createEmployee(1000));   // Director
+console.log(createEmployee('$500')); // Director
